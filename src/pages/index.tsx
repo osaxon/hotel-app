d9945f425a741504925732e594d4bb256eac47f0
Oliver Saxon <oliversaxon@Olivers-MacBook-Air.local>
2023-05-31 10:40:50 +0700

    removed references to example data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,6 @@ import Link from "next/link";
 import { api } from "@/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
@@ -42,9 +40,6 @@ const Home: NextPage = () => {
               </div>
             </Link>
           </div>
-          <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p>
         </div>
       </main>
     </>
